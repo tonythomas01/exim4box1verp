70225f14746f907ccdc5c69401ba9263613d13b5
01tonythomas <tonythomas01@box1verpnop>
2014-06-11 09:37:18 +0000

    Updated the exim4 configs to handle bounce with return address as bounce-

--- script.php
+++ script.php
@@ -1,41 +1,13 @@
-#!/usr/bin/php
 <?php
-// read from stdin
-$fd = fopen("php://stdin", "r");
-$email = "";
-while (!feof($fd)) {
-    $email .= fread($fd, 1024);
-}
-fclose($fd);
-// handle email
-$lines = explode("n", $email);
-
-// empty vars
-$from = "";
-$subject = "";
-$headers = "";
-$message = "";
-$splittingheaders = true;
-for ($i=0; $i<count($lines); $i++) {
-    if ($splittingheaders) {
-        // this is a header
-        $headers .= $lines[$i]."n";
-
-        // look out for special headers
-        if (preg_match("/^Subject: (.*)/", $lines[$i], $matches)) {
-            $subject = $matches[1];
-        }
-        if (preg_match("/^From: (.*)/", $lines[$i], $matches)) {
-            $from = $matches[1];
-        }
-    } else {
-        // not a header, but message
-        $message .= $lines[$i]."n";
-    }
-
-    if (trim($lines[$i])=="") {
-        // empty line, header section has ended
-        $splittingheaders = false;
-    }
-}
+  protected function execute($arguments = array(), $options = array())
+  {
+    // read stdin
+    $content = file_get_contents('php://stdin');
+    $log = fopen('/tmp/log.txt', 'a');
+    fputs($log, $arguments['address'] . "\n");
+    fputs($log, "-----\n");
+    fputs($log, $content);
+    fputs($log, "-----\n\n\n");
+    fclose($log);
+  }
 ?>
