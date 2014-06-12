ee7fddab7da9bc5598110c60e876598893d9d2c6
01tonythomas <tonythomas01@box1verpnop>
2014-06-12 18:15:49 +0000

    Added BHM to handle bounces.

---
+++ bhm/script.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+// read from stdin
+$fd = fopen("php://stdin", "r");
+$eml = "";
+while (!feof($fd)) {
+    $eml .= fread($fd, 1024);
+}
+fclose($fd);
+require_once("bounce_driver.class.php");
+$bouncehandler = new Bouncehandler();
+$bounce = $eml;
+$multiArray = $bouncehandler->get_the_facts($bounce);
+$bounce = $bouncehandler->init_bouncehandler($bounce, 'string');
+list($head, $body) = preg_split("/\r\n\r\n/", $bounce, 2);
+?>
